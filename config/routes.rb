f60e8a42c4cebc6d8c9e8501708d2ff0b9c4247e
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-01 12:44:20 +0000

    [Add]アソシエーション記述しました

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   
   resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
   
+  
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

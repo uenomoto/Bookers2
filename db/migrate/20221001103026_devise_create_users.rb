ff32da6da800987db4c08c4f5881efdd3bd3516e
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-02 14:31:17 +0000

    [Add]カラム不足あり追加しました。

--- db/migrate/20221001103026_devise_create_users.rb
+++ db/migrate/20221001103026_devise_create_users.rb
@@ -33,7 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
       
       t.string :name
-      t.text :introduction
       
       t.timestamps null: false
     end

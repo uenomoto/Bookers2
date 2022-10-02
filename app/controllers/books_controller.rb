1e602a4fb0bc7b61215b351d45e64a31880ca2df
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-02 14:07:45 +0000

    [Add]bookのindexページ右左作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,42 @@
 class BooksController < ApplicationController
+  
   def new
+   @book = Book.new
   end
   
   def create
+   @book = Book.new(book_params)
+   @book.user_id = current_user.id
+   @book.save
+   redirect_to book_path(@book.id)
   end
 
   def index
+   @user = current_user
+   @books = Book.all
+   @book = Book.new
   end
 
   def show
+   @user = current_user
+   @book = Book.find(params[:id])
+   @book = Book.new
   end
 
   def edit
+   @book = Book.find(params[:id])
   end
   
   def update
+   @book = Book.find(params[:id])
+   @book.update(book_params)
+   redirect_to book_path(@book.id)
   end
   
   def destroy
+   @book = Book.find(params[:id])
+   @book.destroy
+   redirect_to books_path
   end
   
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
   end
   
   

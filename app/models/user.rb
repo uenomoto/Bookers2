67c01f6123cd688b715b294fd437a97ce8d9feef
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-03 14:23:18 +0000

    [Add]バリデーションとエラーメッセージ作成しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-   validates :name,presence:true
-         
+   validates :name, length: { minimum: 2, maximum: 20}
+   validates :introduction,length: { maximum: 50}
          
   has_many :books, dependent: :destroy
   

e667da7b71bb2a8212fffd523307947aab49f03e
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-04 14:17:17 +0000

    [Add]Rspec導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     if @user == current_user
     render :edit
     else
-    redirect_to user_path(current_user.)
+    redirect_to user_path(current_user)
     end
   end
   

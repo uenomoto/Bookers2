3cca26067c00c506b1905fc711e657ef6c3fab19
EC2 Default User <ec2-user@ip-172-31-3-199.ap-northeast-1.compute.internal>
2022-10-08 09:34:09 +0000

    [Finish]RSpecでのテスト完了しました！

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -58,9 +58,7 @@ class BooksController < ApplicationController
   private
   
    def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
    end
+  end
   
-  
-end
-
